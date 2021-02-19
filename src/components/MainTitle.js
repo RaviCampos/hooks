e1ec2f453a5e81be9c6e3a5f9cb45e3c3bb622e5
Ravi Campos <rnscampos@gmail.com>
2021-02-18 22:13:09 -0300

    instructions

--- src/components/MainTitle.js
+++ src/components/MainTitle.js
@@ -3,6 +3,7 @@ function MainTitle() {
     return (
         <div className="front-page">
             <h1>Here beggins the mimo world</h1>
+            <h3>Pick an option and get the most recent post index from comoebommorarnoprimeiroandar</h3>
         </div>
     )
 }
